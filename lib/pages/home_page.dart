4e394d9ce80f02cb921e0c2db8d4caf8ff32653a
RJV-YF <freeworld.creator01@gmail.com>
2024-08-14 20:46:37 +0530

    button component created and passed to the box component

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:light_dark_mode/components/box.dart';
+import 'package:light_dark_mode/components/button.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,6 +9,15 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple.shade500,
+      body: Center(
+        child: MyBox(
+          color: Colors.deepPurple.shade300,
+          child: MyButton(
+            color: Colors.deepPurple.shade200,
+            onTap: () {},
+          ),
+        ),
+      ),
     );
   }
 }
